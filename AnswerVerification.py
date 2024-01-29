33b5a38310f2d124b19089755bda616e78cd8c81
DESKTOP-UO1G575\haafe <haaf.emily@gmail.com>
2024-01-29 22:27:03 +0100

    Fix my Bug.
    Reading and outputting the questions and a problem reading the answers

--- AnswerVerification.py
+++ AnswerVerification.py
@@ -1,31 +1,39 @@
-""" import xml.etree.ElementTree as ET
 import xmltodict
 
-# Öffnen Sie die XML-Datei
-with open("testFragen.xml") as file:
-    xml_string = file.read()
+def dynamicQuestion(i):
+    return i
+def dynamicAnswer(j):
+    j=j+1
+    return j
 
-# Konvertieren Sie XML in ein Python-Dictionary
+file = open("fragen.xml", encoding="utf-8")
+xml_string = file.read()
 python_dict = xmltodict.parse(xml_string)
 
-# Iterieren Sie über die Fragen und drucken Sie den Text jeder Frage
-questions = python_dict['questions']['question']
-for question in questions:
-    print("Frage:", question['text'])
 
-# Drucken Sie den allgemeinen Text des Quizgames
-print("Quizgame: Wer wird Informatiker?")
-print(python_dict['questions']['text']) """
+def keys():
+    # Fragen mit Antworten abrufen
+    for i in range(10):
+        # Frage nach dem aktuellen Wert von i
+        question_key = f'question{dynamicQuestion(i)}'
+        
+        # Überprüfen, ob der Schlüssel im Dictionary vorhanden ist
+        if question_key in python_dict['questions']:
+            print(python_dict['questions'][question_key]['text'])
+        else:
+            print(f'Fehler: Der Schlüssel {question_key} wurde nicht gefunden.')
+    # Dynamische Antworten durchlaufen
+        for j in range(4):
+            # Frage nach dem aktuellen Wert von i
+            answer_key = f'answer{dynamicAnswer(j)}'
+            
+            # Überprüfen, ob der Schlüssel im Dictionary vorhanden ist
+            if answer_key in python_dict['questions']:
+                print(python_dict['questions'][question_key][answer_key])
+            else:
+                print(f'Fehler: Der Schlüssel {answer_key} wurde nicht gefunden.')
 
 
-import xml.etree.ElementTree as ET
-import xmltodict
-
-file=open("testFragen.xml")
-xml_string=file.read()
-python_dict=xmltodict.parse(xml_string)
-print(python_dict['questions']['question'])
+print("Hallo zum Quizgame: Wer wird Informatiker?")
+keys()
 file.close()
-    
-print("Quizgame: Wer wird Informatiker?")
-print(python_dict['text'])
