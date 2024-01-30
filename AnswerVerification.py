d03f895acfdda490c2d598a2055d6f5e6ad4ea32
DESKTOP-UO1G575\haafe <haaf.emily@gmail.com>
2024-01-30 21:42:31 +0100

    Fix the bug from output answers

--- AnswerVerification.py
+++ AnswerVerification.py
@@ -1,11 +1,5 @@
 import xmltodict
 
-def dynamicQuestion(i):
-    return i
-def dynamicAnswer(j):
-    j=j+1
-    return j
-
 file = open("fragen.xml", encoding="utf-8")
 xml_string = file.read()
 python_dict = xmltodict.parse(xml_string)
@@ -13,9 +7,9 @@ python_dict = xmltodict.parse(xml_string)
 
 def keys():
     # Fragen mit Antworten abrufen
-    for i in range(10):
+    for i in range(1,10):
         # Frage nach dem aktuellen Wert von i
-        question_key = f'question{dynamicQuestion(i)}'
+        question_key = f'question{(i)}'
         
         # Überprüfen, ob der Schlüssel im Dictionary vorhanden ist
         if question_key in python_dict['questions']:
@@ -23,12 +17,12 @@ def keys():
         else:
             print(f'Fehler: Der Schlüssel {question_key} wurde nicht gefunden.')
     # Dynamische Antworten durchlaufen
-        for j in range(4):
+        for j in range(1,4):
             # Frage nach dem aktuellen Wert von i
-            answer_key = f'answer{dynamicAnswer(j)}'
+            answer_key = f'answer{(j)}'
             
             # Überprüfen, ob der Schlüssel im Dictionary vorhanden ist
-            if answer_key in python_dict['questions']:
+            if answer_key in python_dict['questions'][question_key]:
                 print(python_dict['questions'][question_key][answer_key])
             else:
                 print(f'Fehler: Der Schlüssel {answer_key} wurde nicht gefunden.')
