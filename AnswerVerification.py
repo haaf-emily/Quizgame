155396599835e8416ec23b1667dc41a5ede38705
DESKTOP-UO1G575\haafe <haaf.emily@gmail.com>
2024-02-01 09:59:40 +0100

    Looking for idea for answer verification

--- AnswerVerification.py
+++ AnswerVerification.py
@@ -5,13 +5,13 @@ file = open("fragen.xml", encoding="utf-8")
 xml_string = file.read()
 python_dict = xmltodict.parse(xml_string)
 
+question_key = None
 
 def questionOutput(i):
+    global question_key
     # Fragen mit Antworten abrufen
-    
     # Frage nach dem aktuellen Wert von i
     question_key = f'question{(i)}'
-        
     # Überprüfen, ob der Schlüssel im Dictionary vorhanden ist
     if question_key in python_dict['questions']:
          print(python_dict['questions'][question_key]['text'])
@@ -22,28 +22,29 @@ def questionOutput(i):
     for j in random.sample(range(1, 5),4):
         # Frage nach dem aktuellen Wert von i
         answer_key = f'answer{(j)}'
-        numberanswer = numberanswer + 1
+        numberanswer += 1
         
         # Überprüfen, ob der Schlüssel im Dictionary vorhanden ist
         if answer_key in python_dict['questions'][question_key]:
             # Ausgabe der Antworten und dient zur Antwortüberprüfung
-            if j==1:
-                print(f"{numberanswer} {python_dict['questions'][question_key][answer_key]}" )
-            elif j==2:
-                print(f"{numberanswer} {python_dict['questions'][question_key][answer_key]}" )
-            elif j==3:
-                print(f"{numberanswer} {python_dict['questions'][question_key][answer_key]}")
-            else:
-                print(f"{numberanswer} {python_dict['questions'][question_key][answer_key]}" )
+            print(f"{numberanswer} {python_dict['questions'][question_key][answer_key]}" )
+            if j==5:
+                return python_dict['questions'][question_key][answer_key]
         else:
             print(f'Fehler: Der Schlüssel {answer_key} wurde nicht gefunden.')
 
 
-def responseCheck():
-    print("Hi")
+def responseCheck(masteranswer):
+    user_input = input("Bitte geben Sie von Ihre Antwort die Zahlen ein: ")
+    """ respons_key = f'question{(user_input)}'
+
+    # Überprüfen, ob die Nutzereingabe mit den erwarteten Antworten übereinstimmt
+    if python_dict['questions'][question_key][respons_key] == masteranswer:
+        print("Richtig! Die Antworten sind korrekt.")
+    else:
+        print("Falsch! Die Antworten sind nicht korrekt.") """
 
 print("Hallo zum Quizgame: Wer wird Informatiker?")
-questionOutput(i=1)
-print('Bitte gebe o')
-responseCheck()
+masteranswer= questionOutput(i=1)
+responseCheck(masteranswer)
 file.close()
