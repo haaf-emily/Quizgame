0fefe8e49ce39a3e3879b867a5d1ccb1a96cfd8c
theresita28 <152874041+theresita28@users.noreply.github.com>
2024-02-17 09:45:59 +0100

    Update AnswerVerification
     Code kommentiert

--- AnswerVerification.py
+++ AnswerVerification.py
@@ -3,23 +3,26 @@ import random
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-#Schnittstelle Python/typescript
+# Schnittstelle Python/typescript
 app = Flask(__name__)
 CORS(app,supports_credentials=True)
-# Einlesen von der XML Datei
+
+# Schnittstelle Python/XMl-Datei
 file = open("fragen.xml", encoding="utf-8")
 xml_string = file.read()
-python_dict = xmltodict.parse(xml_string)
+python_dict = xmltodict.parse(xml_string)   # Umwandlung XML-Datei in Dictionary
 
 question_key = None
 
+# Zuweisung was ausgeführt werden soll bei HTTP-Request Get
 
 @app.route('/api/questionOutput', methods=['GET'])
 def questionOutput():
-    #i = 5
 
     message = 'Herzlich Willkommen zu Wer wird Informatiker?'   # Test auf Webseite das eine Verbindung da ist
+    # Dynamische Variable für die von i ausgewählte Frage
     global question_key
+    # Zufällige Auswahl der Fragen (aktuell gibt es 10 mögliche Fragen)
     i = random.sample(range(1, 11), 1)[0]
     
     
@@ -28,27 +31,22 @@ def questionOutput():
     question_key = f'question{(i)}'
     # Überprüfen, ob der Schlüssel im Dictionary vorhanden ist
     if question_key in python_dict['questions']:
-         #print(python_dict['questions'][question_key]['text'])
          questiontext= python_dict['questions'][question_key]['text']
-         #return jsonify(question = questiontext)
     else:
         print(f'Fehler: Der Schlüssel {question_key} wurde nicht gefunden.')
 
 
-    # Dynamische Antworten durchlaufen
+    # Auswahl dynamischer Antworten über j 
     numberanswer = 0
     for j in random.sample(range(1, 5),4):
-        # Frage nach dem aktuellen Wert von i
+        # Frage nach dem aktuellen Wert von j
         answer_key = f'answer{(j)}'
+        #Benötigt man für die Zuweisung für die Buttons
         numberanswer += 1
         
         # Überprüfen, ob der Schlüssel im Dictionary vorhanden ist
         if answer_key in python_dict['questions'][question_key]:
             # Ausgabe der Antworten und dient zur Antwortüberprüfung
-            #print(f"{numberanswer} {python_dict['questions'][question_key][answer_key]}" )
-            #answer = python_dict['questions'][question_key][answer_key]
-
-            # Ausgabe der Antworten
             if numberanswer==1:
                 answer1=python_dict['questions'][question_key][answer_key]
             elif numberanswer==2:
@@ -60,6 +58,7 @@ def questionOutput():
         else:
             print(f'Fehler: Der Schlüssel {answer_key} wurde nicht gefunden.')
     
+    # Zuweisung der Wertübergabe
     data = {
             'message': message,
             'question': questiontext,
@@ -68,21 +67,20 @@ def questionOutput():
             'answer3': answer3,
             'answer4': answer4,
     }
+    # Wertübergabe an Schnittstelle
     return jsonify(data)
+
+# Zuweisung was ausgeführt werden soll bei HTTP-Request POST und OPTIONS
       
-@app.route('/api/responseCheck', methods=['POST', 'OPTIONS'])
+@app.route('/api/responseCheck', methods=['POST', 'OPTIONS'])   
 def responseCheck():
     if request.method == 'OPTIONS':
         # Antwort auf OPTIONS-Anfrage ohne Daten
-    
         return ' ', 204
 
 
     data = request.json  # Zugriff auf die JSON-Daten der Anfrage
     print('Received data:', data)
-    
-
-
     
     # Nur den Wert ("answer") ausgeben
     answer = data.get('selectedAnswer', None)   
@@ -95,14 +93,12 @@ def responseCheck():
     else:
         correct="Falsch"
 
-    print('Value:', answer)    #   Ausgabe in Terminal Value: {'ant1': ' Reelle Zahlen'}
+    # Variable correct für User, damit er sieht ob die Antwort richtig oder falsch ist
+        
     print(python_dict['questions'][question_key]['answer4'])
     return jsonify(correct = f'{correct}')
     
 
-  
-
-#questionOutput(1)
+# Zuweisung auf welchen Port Flask läuft  
 if __name__ == '__main__':
     app.run(port=5000)
-
